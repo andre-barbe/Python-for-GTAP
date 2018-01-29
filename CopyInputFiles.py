8c3822e5e32b504e36bafeeb6dafa7f5619a79de
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-01-29 15:58:18 -0500

    Renamed GTAP Directory and TABLOed GTAPv7 for use through command line

--- CopyInputFiles.py
+++ CopyInputFiles.py
@@ -1,7 +1,7 @@
 __author__ = "Andre Barbe"
 __project__ = "Python for GTAP"
 __created__ = "2018-1-25"
-__altered__ = "2018-1-25"
+__altered__ = "2018-1-29"
 
 import shutil
 
@@ -14,19 +14,19 @@ class CopyInputFiles(object):
     def create(self) -> None:
         #Define lists of files to copy.
         list_gtap_gemsim = [
-            "gtap.tab",
-            "gtap.gss",
-            "gtap.gst",
-            "gtap.min"
+            "gtapv7.tab",
+            "gtapv7.gss",
+            "gtapv7.gst",
+            "gtapv7.min"
             # "gtap.cmf",
         ]
 
         list_shocks_gemsim = [
-            "shocks.tab",
-            "shocks.gss",
-            "shocks.gst",
-            "shocks.min",
-            "shocks.cmf"
+            # "shocks.tab",
+            # "shocks.gss",
+            # "shocks.gst",
+            # "shocks.min",
+            # "shocks.cmf"
         ]
 
         list_gtap_data = [
@@ -34,16 +34,18 @@ class CopyInputFiles(object):
             "baserate.har",
             "baseview.har",
             "sets.har",
-            "co2.har",
             "default.prm"
         ]
 
         list_shocks_data = [
-            ""
+
         ]
 
-        list_of_files_to_copy = list_gtap_gemsim + list_gtap_data + list_shocks_gemsim + list_shocks_data
+        list_of_files_to_copy = list_gtap_gemsim\
+                                + list_gtap_data\
+                                + list_shocks_gemsim\
+                                + list_shocks_data
 
         for file_name in list_of_files_to_copy:
             # copy files with file_name from Input_Files to Work_Files
-            shutil.copy('Input_Files\\{0}'.format(file_name), 'Work_Files\\{0}'.format(file_name))
+            shutil.copy('Input_Files\\GTAP\\{0}'.format(file_name), 'Work_Files\\{0}'.format(file_name))
--- Main.py
+++ Main.py
@@ -5,9 +5,11 @@ __altered__ = "2018-1-25"
 
 # Import Methods
 from Cleanup import Cleanup
+from CopyInputFiles import CopyInputFiles
 
 # Setup Files
 Cleanup().create()
+CopyInputFiles().create()
 
 # Run Simulations
 
